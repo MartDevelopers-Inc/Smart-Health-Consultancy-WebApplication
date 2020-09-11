f431e82439f17a81f6534fce2a85113934014c0a
MartMbithi <martdevelopers254@gmail.com>
2020-09-11 14:06:46 +0300

    Cranked Payments Migrations

--- database/migrations/2020_09_11_104942_create_membership_payments_table.php
+++ database/migrations/2020_09_11_104942_create_membership_payments_table.php
@@ -14,7 +14,13 @@ class CreateMembershipPaymentsTable extends Migration
     public function up()
     {
         Schema::create('membership_payments', function (Blueprint $table) {
-            $table->id();
+            $table->string('pay_id')->id();
+            $table->string('pay_code');
+            $table->string('pay_method');
+            $table->string('pay_amt');
+            $table->string('member_id');
+            $table->string('member_package');
+            $table->string('member_name');
             $table->timestamps();
         });
     }
