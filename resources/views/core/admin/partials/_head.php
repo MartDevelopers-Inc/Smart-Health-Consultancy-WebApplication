6214ae3e32041b7d5118e9234e07767c96ba371f
MartMbithi <martdevelopers254@gmail.com>
2020-09-11 14:41:26 +0300

    Auth Front End - Sign In Page

---
+++ resources/views/core/admin/partials/_head.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, shrink-to-fit=no">
+    <title>Smart Health Consultancy Inc - Craddle Of Health Care Services</title>
+    <link rel="icon" type="image/x-icon" href="assets/img/favicon.ico" />
+    <link href="https://fonts.googleapis.com/css?family=Nunito:400,600,700" rel="stylesheet">
+    <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
+    <link href="assets/css/plugins.css" rel="stylesheet" type="text/css" />
+    <!--Auth CSS Files-->
+    <link href="assets/css/authentication/form-2.css" rel="stylesheet" type="text/css" />
+    <link rel="stylesheet" type="text/css" href="assets/css/forms/theme-checkbox-radio.css">
+    <link rel="stylesheet" type="text/css" href="assets/css/forms/switches.css">
+    
+
+</head>
---
+++ resources/views/core/admin/partials/_scripts.php
@@ -0,0 +1,8 @@
+<!-- BEGIN GLOBAL MANDATORY SCRIPTS -->
+<script src="assets/js/libs/jquery-3.1.1.min.js"></script>
+<script src="bootstrap/js/popper.min.js"></script>
+<script src="bootstrap/js/bootstrap.min.js"></script>
+
+<!--Auth Js -->
+<script src="assets/js/authentication/form-2.js"></script>
+
