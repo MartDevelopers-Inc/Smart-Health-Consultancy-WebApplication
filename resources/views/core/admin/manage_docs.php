8858ca86b42a6e9a04874f787b407bc361ca492e
MartMbithi <martdevelopers254@gmail.com>
2020-09-12 21:35:21 +0300

    Implemented Front End On Import .XLS File

--- resources/views/core/admin/manage_docs.php
+++ resources/views/core/admin/manage_docs.php
@@ -66,6 +66,15 @@ require_once('partials/_head.php');
                                 </svg>
                                 Add New Medical Expert
                             </a>
+
+                            <a class="btn btn-outline-primary" href="add_medical_expert.php">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-activity">
+                                    <path d="M16 21v-2a4 4 0 0 0-4-4H5a4 4 0 0 0-4 4v2"></path>
+                                    <circle cx="8.5" cy="7" r="4"></circle>
+                                    <polyline points="17 11 19 13 23 9"></polyline>
+                                </svg>
+                                Import .XLS Document
+                            </a>
                             <div class="table-responsive mb-4 mt-4">
                                 <table id="zero-config" class="table table-hover" style="width:100%" style="width:100%">
                                     <thead>
@@ -88,18 +97,18 @@ require_once('partials/_head.php');
 
                                             <td>
                                                 <div class="btn-group">
-                                                    <button type="button" class="btn btn-dark btn-sm">Open</button>
+                                                    <button type="button" class="btn btn-dark btn-sm">Manage Doctor</button>
                                                     <button type="button" class="btn btn-dark btn-sm dropdown-toggle dropdown-toggle-split" id="dropdownMenuReference1" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" data-reference="parent">
                                                         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-chevron-down">
                                                             <polyline points="6 9 12 15 18 9"></polyline>
                                                         </svg>
                                                     </button>
                                                     <div class="dropdown-menu" aria-labelledby="dropdownMenuReference1">
-                                                        <a class="dropdown-item" href="#">Action</a>
-                                                        <a class="dropdown-item" href="#">Another action</a>
-                                                        <a class="dropdown-item" href="#">Something else here</a>
+                                                        <a class="dropdown-item" href="view_doc.php?view">View Account</a>
+                                                        <a class="dropdown-item" href="update_doc.php?update">Update Account</a>
+                                                        <a class="dropdown-item" href="verify_doc.php?verify">Verify Account</a>
                                                         <div class="dropdown-divider"></div>
-                                                        <a class="dropdown-item" href="#">Separated link</a>
+                                                        <a class="dropdown-item text-danger" href="manage_docs.php?delete">Delete Account</a>
                                                     </div>
                                                 </div>
                                             </td>
@@ -110,7 +119,6 @@ require_once('partials/_head.php');
                             </div>
                         </div>
                     </div>
-
                 </div>
 
             </div>
