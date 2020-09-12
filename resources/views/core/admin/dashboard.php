43e26713c03e7805ec7010ae630190874ccc3c2b
MartMbithi <martdevelopers254@gmail.com>
2020-09-12 13:00:06 +0300

    Minor Refactors

--- resources/views/core/admin/dashboard.php
+++ resources/views/core/admin/dashboard.php
@@ -37,8 +37,8 @@ check_login();
 
                         <nav class="breadcrumb-one" aria-label="breadcrumb">
                             <ol class="breadcrumb">
-                                <li class="breadcrumb-item"><a href="javascript:void(0);">Dashboard</a></li>
-                                <li class="breadcrumb-item active" aria-current="page"><span>Sales</span></li>
+                                <li class="breadcrumb-item"><a href="javascript:void(0);">Home</a></li>
+                                <li class="breadcrumb-item active" aria-current="page"><span>Main Dashboard</span></li>
                             </ol>
                         </nav>
 
--- resources/views/core/admin/partials/_sidebar.php
+++ resources/views/core/admin/partials/_sidebar.php
@@ -4,7 +4,7 @@
         <div class="shadow-bottom"></div>
         <ul class="list-unstyled menu-categories" id="accordionExample">
             <li class="menu">
-                <a href="#dashboard" data-active="true" data-toggle="collapse" aria-expanded="true" class="dropdown-toggle">
+                <a href="#dashboard" data-active="false" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle">
                     <div class="">
                         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-home">
                             <path d="M3 9l9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z"></path>
@@ -18,8 +18,8 @@
                         </svg>
                     </div>
                 </a>
-                <ul class="collapse submenu list-unstyled show" id="dashboard" data-parent="#accordionExample">
-                    <li class="active">
+                <ul class="collapse submenu list-unstyled" id="dashboard" data-parent="#accordionExample">
+                    <li class="">
                         <a href="dashboard.php"> Main Dashboard </a>
                     </li>
                     <li>
