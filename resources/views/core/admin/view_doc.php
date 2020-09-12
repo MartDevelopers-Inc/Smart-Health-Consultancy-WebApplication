5c1a0951e36035365e4ad3822b5c9ce422817f09
MartMbithi <martdevelopers254@gmail.com>
2020-09-13 01:31:25 +0300

    Cranked Views On Medical Expert

--- resources/views/core/admin/view_doc.php
+++ resources/views/core/admin/view_doc.php
@@ -16,6 +16,12 @@ require_once('partials/_head.php');
     $stmt->execute(); //ok
     $res = $stmt->get_result();
     while ($row = $res->fetch_object()) {
+        if ($row->doc_photo == '') {
+            //Load Default Image
+            $profile = "<img src='assets/img/admin.png' class='img-fluid img-thumbnail' alt='avatar'>";
+        } else {
+            $profile = "<img src='assets/img/paramedics/$row->doc_photo' class='img-fluid img-thumbnail' alt='avatar'>";
+        }
 
     ?>
         <!--  END NAVBAR  -->
@@ -78,8 +84,8 @@ require_once('partials/_head.php');
                                             </svg></a>
                                     </div>
                                     <div class="text-center user-info">
-                                        <img src='assets/img/profile-3.jpg' class='img-fluid img-thumbnail' alt='avatar'>
-                                        <p class="">Jimmy Turner</p>
+                                        <?php echo $profile; ?>
+                                        <p class=""><?php echo $row->doc_name; ?></p>
                                     </div>
                                     <div class="user-info-list">
 
@@ -92,7 +98,7 @@ require_once('partials/_head.php');
                                                         <line x1="6" y1="1" x2="6" y2="4"></line>
                                                         <line x1="10" y1="1" x2="10" y2="4"></line>
                                                         <line x1="14" y1="1" x2="14" y2="4"></line>
-                                                    </svg> Web Developer
+                                                    </svg> <?php echo $row->doc_number; ?>
                                                 </li>
                                                 <li class="contacts-block__item">
                                                     <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-calendar">
@@ -100,59 +106,28 @@ require_once('partials/_head.php');
                                                         <line x1="16" y1="2" x2="16" y2="6"></line>
                                                         <line x1="8" y1="2" x2="8" y2="6"></line>
                                                         <line x1="3" y1="10" x2="21" y2="10"></line>
-                                                    </svg>Jan 20, 1989
+                                                    </svg> <?php echo $row->doc_status; ?>
                                                 </li>
+
                                                 <li class="contacts-block__item">
-                                                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-map-pin">
-                                                        <path d="M21 10c0 7-9 13-9 13s-9-6-9-13a9 9 0 0 1 18 0z"></path>
-                                                        <circle cx="12" cy="10" r="3"></circle>
-                                                    </svg>New York, USA
-                                                </li>
-                                                <li class="contacts-block__item">
-                                                    <a href="mailto:example@mail.com"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-mail">
+                                                    <a href="mailto:<?php echo $row->doc_email; ?>"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-mail">
                                                             <path d="M4 4h16c1.1 0 2 .9 2 2v12c0 1.1-.9 2-2 2H4c-1.1 0-2-.9-2-2V6c0-1.1.9-2 2-2z"></path>
                                                             <polyline points="22,6 12,13 2,6"></polyline>
-                                                        </svg>Jimmy@gmail.com</a>
+                                                        </svg><?php echo $row->doc_email; ?></a>
                                                 </li>
                                                 <li class="contacts-block__item">
                                                     <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-phone">
                                                         <path d="M22 16.92v3a2 2 0 0 1-2.18 2 19.79 19.79 0 0 1-8.63-3.07 19.5 19.5 0 0 1-6-6 19.79 19.79 0 0 1-3.07-8.67A2 2 0 0 1 4.11 2h3a2 2 0 0 1 2 1.72 12.84 12.84 0 0 0 .7 2.81 2 2 0 0 1-.45 2.11L8.09 9.91a16 16 0 0 0 6 6l1.27-1.27a2 2 0 0 1 2.11-.45 12.84 12.84 0 0 0 2.81.7A2 2 0 0 1 22 16.92z"></path>
-                                                    </svg> +1 (530) 555-12121
+                                                    </svg> <?php echo $row->doc_phone; ?>
                                                 </li>
-                                                <li class="contacts-block__item">
-                                                    <ul class="list-inline">
-                                                        <li class="list-inline-item">
-                                                            <div class="social-icon">
-                                                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-facebook">
-                                                                    <path d="M18 2h-3a5 5 0 0 0-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 0 1 1-1h3z"></path>
-                                                                </svg>
-                                                            </div>
-                                                        </li>
-                                                        <li class="list-inline-item">
-                                                            <div class="social-icon">
-                                                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-twitter">
-                                                                    <path d="M23 3a10.9 10.9 0 0 1-3.14 1.53 4.48 4.48 0 0 0-7.86 3v1A10.66 10.66 0 0 1 3 4s-4 9 5 13a11.64 11.64 0 0 1-7 2c9 5 20 0 20-11.5a4.5 4.5 0 0 0-.08-.83A7.72 7.72 0 0 0 23 3z"></path>
-                                                                </svg>
-                                                            </div>
-                                                        </li>
-                                                        <li class="list-inline-item">
-                                                            <div class="social-icon">
-                                                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-linkedin">
-                                                                    <path d="M16 8a6 6 0 0 1 6 6v7h-4v-7a2 2 0 0 0-2-2 2 2 0 0 0-2 2v7h-4v-7a6 6 0 0 1 6-6z"></path>
-                                                                    <rect x="2" y="9" width="4" height="12"></rect>
-                                                                    <circle cx="4" cy="4" r="2"></circle>
-                                                                </svg>
-                                                            </div>
-                                                        </li>
-                                                    </ul>
-                                                </li>
+
                                             </ul>
                                         </div>
                                     </div>
                                 </div>
                             </div>
 
-                            <div class="education layout-spacing ">
+                            <!-- <div class="education layout-spacing ">
                                 <div class="widget-content widget-content-area">
                                     <h3 class="">Education</h3>
                                     <div class="timeline-alter">
@@ -191,9 +166,9 @@ require_once('partials/_head.php');
                                         </div>
                                     </div>
                                 </div>
-                            </div>
+                            </div> -->
 
-                            <div class="work-experience layout-spacing ">
+                            <!-- <div class="work-experience layout-spacing ">
 
                                 <div class="widget-content widget-content-area">
 
@@ -240,13 +215,13 @@ require_once('partials/_head.php');
                                     </div>
                                 </div>
 
-                            </div>
+                            </div> -->
 
                         </div>
 
                         <div class="col-xl-8 col-lg-6 col-md-7 col-sm-12 layout-top-spacing">
 
-                            <div class="skills layout-spacing ">
+                            <!-- <div class="skills layout-spacing ">
                                 <div class="widget-content widget-content-area">
                                     <h3 class="">Skills</h3>
                                     <div class="progress br-30">
@@ -271,16 +246,13 @@ require_once('partials/_head.php');
                                     </div>
 
                                 </div>
-                            </div>
+                            </div> -->
 
                             <div class="bio layout-spacing ">
                                 <div class="widget-content widget-content-area">
-                                    <h3 class="">Bio</h3>
-                                    <p>I'm Web Developer from California. I code and design websites worldwide. Mauris varius tellus vitae tristique sagittis. Sed aliquet, est nec auctor aliquet, orci ex vestibulum ex, non pharetra lacus erat ac nulla.</p>
-
-                                    <p>Sed vulputate, ligula eget mollis auctor, lectus elit feugiat urna, eget euismod turpis lectus sed ex. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Nunc ut velit finibus, scelerisque sapien vitae, pharetra est. Nunc accumsan ligula vehicula scelerisque vulputate.</p>
-
-                                    <div class="bio-skill-box">
+                                    <h3 class="">Bio </h3>
+                                    <?php echo $row->doc_bio; ?>
+                                    <!-- <div class="bio-skill-box">
 
                                         <div class="row">
 
@@ -338,17 +310,14 @@ require_once('partials/_head.php');
 
                                         </div>
 
-                                    </div>
-
+                                    </div> -->
                                 </div>
                             </div>
-
                         </div>
-
                     </div>
                 </div>
             <?php require_once('partials/_footer.php');
-            } ?>
+        } ?>
             </div>
             <!--  END CONTENT AREA  -->
         </div>
