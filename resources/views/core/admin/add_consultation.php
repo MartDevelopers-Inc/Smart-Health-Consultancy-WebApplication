2922ecccae6aab8cdec0c8fa096b2fecff5951c8
MartMbithi <martdevelopers254@gmail.com>
2020-09-15 16:24:51 +0300

    Add Consultation -001

---
+++ resources/views/core/admin/add_consultation.php
@@ -0,0 +1,149 @@
+<?php
+session_start();
+require_once('configs/config.php');
+require_once('configs/checklogin.php');
+
+
+require_once('partials/_head.php');
+?>
+
+<body>
+
+    <!--  BEGIN NAVBAR  -->
+    <?php require_once('partials/_nav.php'); ?>
+    <!--  END NAVBAR  -->
+
+    <!--  BEGIN NAVBAR  -->
+    <div class="sub-header-container">
+        <header class="header navbar navbar-expand-sm">
+            <a href="javascript:void(0);" class="sidebarCollapse" data-placement="bottom"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-menu">
+                    <line x1="3" y1="12" x2="21" y2="12"></line>
+                    <line x1="3" y1="6" x2="21" y2="6"></line>
+                    <line x1="3" y1="18" x2="21" y2="18"></line>
+                </svg></a>
+
+            <ul class="navbar-nav flex-row">
+                <li>
+                    <div class="page-header">
+
+                        <nav class="breadcrumb-one" aria-label="breadcrumb">
+                            <ol class="breadcrumb">
+                                <li class="breadcrumb-item"><a href="dashboard.php">Home</a></li>
+                                <li class="breadcrumb-item"><a href="dashboard.php">Dashboard</a></li>
+                                <li class="breadcrumb-item"><a href="manage_consultations.php">Consultations</a></li>
+                                <li class="breadcrumb-item active" aria-current="page"><span>Add Consultation</span></li>
+                            </ol>
+                        </nav>
+
+                    </div>
+                </li>
+            </ul>
+        </header>
+    </div>
+    <!--  END NAVBAR  -->
+
+    <!--  BEGIN MAIN CONTAINER  -->
+    <div class="main-container" id="container">
+
+        <div class="overlay"></div>
+        <div class="search-overlay"></div>
+
+        <!--  BEGIN SIDEBAR  -->
+        <div class="sidebar-wrapper sidebar-theme">
+
+            <?php require_once('partials/_sidebar.php'); ?>
+
+        </div>
+        <!--  END SIDEBAR  -->
+
+        <!--  BEGIN CONTENT AREA  -->
+        <div id="content" class="main-content">
+            <div class="layout-px-spacing">
+
+                <div class="row layout-top-spacing">
+                    <div class="col-xl-12 col-lg-12 col-sm-12  layout-spacing">
+                        <div class="widget-content widget-content-area br-6">
+
+                            <!-- <a class="btn btn-outline-success" href="add_client.php">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-activity">
+                                    <path d="M16 21v-2a4 4 0 0 0-4-4H5a4 4 0 0 0-4 4v2"></path>
+                                    <circle cx="8.5" cy="7" r="4"></circle>
+                                    <line x1="20" y1="8" x2="20" y2="14"></line>
+                                    <line x1="23" y1="11" x2="17" y2="11"></line>
+                                </svg>
+                                Register New Client
+                            </a> -->
+
+                            <!-- <a class="btn btn-outline-primary" href="">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-activity">
+                                    <path d="M16 21v-2a4 4 0 0 0-4-4H5a4 4 0 0 0-4 4v2"></path>
+                                    <circle cx="8.5" cy="7" r="4"></circle>
+                                    <polyline points="17 11 19 13 23 9"></polyline>
+                                </svg>
+                                Import .XLS Document
+                            </a> -->
+                            <div class="table-responsive mb-4 mt-4">
+                                <table id="zero-config" class="table table-hover" style="width:100%" style="width:100%">
+                                    <thead>
+                                        <tr>
+                                            <th>Name</th>
+                                            <th>Phone</th>
+                                            <th>Email</th>
+                                            <th>Package</th>
+                                            <th>Joined On</th>
+                                            <th>Action</th>
+                                        </tr>
+                                    </thead>
+
+                                    <tbody>
+                                        <?php
+                                        $ret = "SELECT * FROM `members` ";
+                                        $stmt = $mysqli->prepare($ret);
+                                        $stmt->execute(); //ok
+                                        $res = $stmt->get_result();
+                                        while ($row = $res->fetch_object()) {
+                                        ?>
+                                            <tr>
+                                                <td><?php echo $row->member_name; ?></td>
+                                                <td><?php echo $row->member_phone; ?></td>
+                                                <td><?php echo $row->member_email; ?></td>
+                                                <td>
+                                                    <?php
+                                                    if ($row->member_package == 'Gold Package') {
+                                                        echo "<span class='badge outline-badge-success'>$row->member_package</span>";
+                                                    } elseif ($row->member_package == 'Silver Package') {
+                                                        echo "<span class='badge outline-badge-warning'>$row->member_package</span>";
+                                                    } elseif ($row->member_package == 'Bronze Package') {
+                                                        echo "<span class='badge outline-badge-primary'>$row->member_package</span>";
+                                                    } else {
+                                                        echo "<span class='badge outline-badge-danger'>$row->member_package</span>";
+                                                    }
+                                                    ?>
+                                                </td>
+                                                <td><?php echo date('d M Y g:i', strtotime($row->created_at)); ?></td>
+                                                <td>
+                                                    <a class="badge outline-badge-success" href="add_client_consultation.php?member_id =<?php echo $row->member_id;?>&member_name=<?php echo $row->member_name;?>&member_phone=<?php echo $row->member_phone;?>">
+                                                        Add Consultation
+                                                    </a>
+                                                </td>
+                                            </tr>
+                                        <?php
+                                        } ?>
+
+                                    </tbody>
+                                </table>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+            <?php require_once('partials/_footer.php'); ?>
+        </div>
+        <!--  END CONTENT AREA  -->
+    </div>
+    <!-- END MAIN CONTAINER -->
+    <?php require_once('partials/_scripts.php'); ?>
+</body>
+
+</html>
