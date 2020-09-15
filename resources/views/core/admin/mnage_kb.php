5a075d40972a58600ba91cbabcf683acc2700cec
MartMbithi <martdevelopers254@gmail.com>
2020-09-15 14:15:35 +0300

    Updated KB Approach

--- resources/views/core/admin/mnage_kb.php
+++ resources/views/core/admin/mnage_kb.php
@@ -3,26 +3,24 @@ session_start();
 require_once('configs/config.php');
 require_once('configs/checklogin.php');
 
-//Delete
+//Delete Payroll
 if (isset($_GET['delete'])) {
-    $kb_id = $_GET['delete'];
+    $delete = $_GET['delete'];
     $adn = "DELETE FROM knowledge_base WHERE kb_id =?";
     $stmt = $mysqli->prepare($adn);
-    $stmt->bind_param('s', $kb_id);
+    $stmt->bind_param('s', $delete);
     $stmt->execute();
     $stmt->close();
     if ($stmt) {
         $success = "Deleted" && header("refresh:1; url=mnage_kb.php");
     } else {
-        //inject alert that task failed
         $info = "Please Try Again Or Try Later";
     }
 }
-
 require_once('partials/_head.php');
 ?>
 
-<body data-spy="scroll" data-target="#navSection" data-offset="140">
+<body>
 
     <!--  BEGIN NAVBAR  -->
     <?php require_once('partials/_nav.php'); ?>
@@ -45,8 +43,8 @@ require_once('partials/_head.php');
                             <ol class="breadcrumb">
                                 <li class="breadcrumb-item"><a href="dashboard.php">Home</a></li>
                                 <li class="breadcrumb-item"><a href="dashboard.php">Dashboard</a></li>
-                                <li class="breadcrumb-item"><a href="create_kb.php">Knowledge Base</a></li>
-                                <li class="breadcrumb-item active" aria-current="page"><span>Manage Knowlegde Base</span></li>
+                                <li class="breadcrumb-item"><a href="dashboard.php">Knowlegde Base</a></li>
+                                <li class="breadcrumb-item active" aria-current="page"><span>Manage KB</span></li>
                             </ol>
                         </nav>
 
@@ -64,7 +62,11 @@ require_once('partials/_head.php');
         <div class="search-overlay"></div>
 
         <!--  BEGIN SIDEBAR  -->
-        <?php require_once('partials/_sidebar.php'); ?>
+        <div class="sidebar-wrapper sidebar-theme">
+
+            <?php require_once('partials/_sidebar.php'); ?>
+
+        </div>
         <!--  END SIDEBAR  -->
 
         <!--  BEGIN CONTENT AREA  -->
@@ -73,59 +75,82 @@ require_once('partials/_head.php');
 
                 <div class="row layout-top-spacing">
                     <div class="col-xl-12 col-lg-12 col-sm-12  layout-spacing">
-                        <div id="iconsAccordion" class="accordion-icons">
-                            <?php
-                            $ret = "SELECT * FROM `knowledge_base` ";
-                            $stmt = $mysqli->prepare($ret);
-                            $stmt->execute(); //ok
-                            $res = $stmt->get_result();
-                            while ($row = $res->fetch_object()) {
-                            ?>
-                                <div class="card">
-                                    <div class="card-header" id="headingOne3">
-                                        <section class="mb-0 mt-0">
-                                            <div role="menu" class="collapsed" data-toggle="collapse" data-target="#<?php echo $row->kb_id; ?>" aria-expanded="true" aria-controls="iconAccordionOne">
-                                                <div class="accordion-icon">
-                                                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-activity">
-                                                        <rect x="4" y="4" width="16" height="16" rx="2" ry="2"></rect>
-                                                        <rect x="9" y="9" width="6" height="6"></rect>
-                                                        <line x1="9" y1="1" x2="9" y2="4"></line>
-                                                        <line x1="15" y1="1" x2="15" y2="4"></line>
-                                                        <line x1="9" y1="20" x2="9" y2="23"></line>
-                                                        <line x1="15" y1="20" x2="15" y2="23"></line>
-                                                        <line x1="20" y1="9" x2="23" y2="9"></line>
-                                                        <line x1="20" y1="14" x2="23" y2="14"></line>
-                                                        <line x1="1" y1="9" x2="4" y2="9"></line>
-                                                        <line x1="1" y1="14" x2="4" y2="14"></line>
-                                                    </svg>
-                                                </div>
-                                                <?php echo $row->kb_title; ?>
-                                                <div class="icons">
-                                                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-chevron-down">
-                                                        <polyline points="6 9 12 15 18 9"></polyline>
-                                                    </svg>
-                                                </div>
-                                            </div>
-                                        </section>
-                                    </div>
+                        <div class="widget-content widget-content-area br-6">
 
-                                    <div id="<?php echo $row->kb_id; ?>" class="collapse" aria-labelledby="headingOne3" data-parent="#iconsAccordion">
-                                        <div class="card-body">
-                                            <p class="">
-                                                <?php echo $row->kb_desc; ?>
-                                            </p>
-                                        </div>
-                                    </div>
-                                </div>
-                            <?php } ?>
+                            <a class="btn btn-outline-success" href="create_kb.php">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-activity">
+                                    <rect x="4" y="4" width="16" height="16" rx="2" ry="2"></rect>
+                                    <rect x="9" y="9" width="6" height="6"></rect>
+                                    <line x1="9" y1="1" x2="9" y2="4"></line>
+                                    <line x1="15" y1="1" x2="15" y2="4"></line>
+                                    <line x1="9" y1="20" x2="9" y2="23"></line>
+                                    <line x1="15" y1="20" x2="15" y2="23"></line>
+                                    <line x1="20" y1="9" x2="23" y2="9"></line>
+                                    <line x1="20" y1="14" x2="23" y2="14"></line>
+                                    <line x1="1" y1="9" x2="4" y2="9"></line>
+                                    <line x1="1" y1="14" x2="4" y2="14"></line>
+                                </svg>
+
+                                Create New Diseases Knowledge Base
+                            </a>
+
+                            <!--<a class="btn btn-outline-primary" href="">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-activity">
+                                    <path d="M16 21v-2a4 4 0 0 0-4-4H5a4 4 0 0 0-4 4v2"></path>
+                                    <circle cx="8.5" cy="7" r="4"></circle>
+                                    <polyline points="17 11 19 13 23 9"></polyline>
+                                </svg>
+                                Import .XLS Document
+                            </a>-->
+                            <div class="table-responsive mb-4 mt-4">
+                                <table id="zero-config" class="table table-hover" style="width:100%" style="width:100%">
+                                    <thead>
+                                        <tr>
+                                            <th>KB Code</th>
+                                            <th>KB Title</th>
+                                            <th>Generated At</th>
+                                            <th>Action</th>
+                                        </tr>
+                                    </thead>
+
+                                    <tbody>
+                                        <?php
+                                        $ret = "SELECT * FROM `knowledge_base` ";
+                                        $stmt = $mysqli->prepare($ret);
+                                        $stmt->execute(); //ok
+                                        $res = $stmt->get_result();
+                                        while ($row = $res->fetch_object()) {
+                                        ?>
+                                            <tr>
+                                                <td>
+                                                    <a class="badge outline-badge-success" href="view_kb.php?view=<?php echo $row->kb_id; ?>">
+                                                        <?php echo $row->kb_id; ?>
+                                                    </a>
+                                                </td>
+                                                <td><?php echo $row->kb_title; ?></td>
+                                                <td><?php echo date('d M Y g:i', strtotime($row->created_at)); ?></td>
+                                                <td>
+                                                    <a class="badge outline-badge-primary" href="update_kb.php?update=<?php echo $row->kb_id; ?>">Update</a>
+
+                                                    <a class="badge outline-badge-danger text-danger" href="mnage_kb.php?delete=<?php echo $row->kb_id; ?>">Delete</a>
+                                                </td>
+                                            </tr>
+                                        <?php
+                                        } ?>
+                                    </tbody>
+                                </table>
+                            </div>
                         </div>
                     </div>
                 </div>
-            </div>
-        </div>
-    </div>
-   
-    <!--  END CONTENT AREA  -->
-    <?php require_once('partials/_scripts.php'); ?>
 
-    </html>
+            </div>
+            <?php require_once('partials/_footer.php'); ?>
+        </div>
+        <!--  END CONTENT AREA  -->
+    </div>
+    <!-- END MAIN CONTAINER -->
+    <?php require_once('partials/_scripts.php'); ?>
+</body>
+
+</html>
