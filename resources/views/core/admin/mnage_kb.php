4602a848243b9fe085adb236333f6d11dca6ff94
MartMbithi <martdevelopers254@gmail.com>
2020-09-15 14:00:07 +0300

    Cranked Manage On Knowledge Base

--- resources/views/core/admin/mnage_kb.php
+++ resources/views/core/admin/mnage_kb.php
@@ -73,51 +73,51 @@ require_once('partials/_head.php');
 
                 <div class="row layout-top-spacing">
                     <div class="col-xl-12 col-lg-12 col-sm-12  layout-spacing">
-                        <div class="widget-content widget-content-area br-6">
-                            <div id="iconsAccordion" class="accordion-icons">
+                        <div id="iconsAccordion" class="accordion-icons">
+                            <?php
+                            $ret = "SELECT * FROM `knowledge_base` ";
+                            $stmt = $mysqli->prepare($ret);
+                            $stmt->execute(); //ok
+                            $res = $stmt->get_result();
+                            while ($row = $res->fetch_object()) {
+                            ?>
                                 <div class="card">
-                                    <div class="card-header" id="headingOne1">
+                                    <div class="card-header" id="headingOne3">
                                         <section class="mb-0 mt-0">
-                                            <div role="menu" class="collapsed" data-toggle="collapse" data-target="#defaultAccordionOne" aria-expanded="true" aria-controls="defaultAccordionOne">
-                                                Collapsible Group Item #1 <div class="icons"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-chevron-down">
+                                            <div role="menu" class="collapsed" data-toggle="collapse" data-target="#<?php echo $row->kb_id;?>" aria-expanded="true" aria-controls="iconAccordionOne">
+                                                <div class="accordion-icon">
+                                                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-airplay">
+                                                        <path d="M5 17H4a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h16a2 2 0 0 1 2 2v10a2 2 0 0 1-2 2h-1"></path>
+                                                        <polygon points="12 15 17 21 7 21 12 15"></polygon>
+                                                    </svg>
+                                                </div>
+                                                <?php echo $row->kb_title;?>
+                                                <div class="icons">
+                                                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-chevron-down">
                                                         <polyline points="6 9 12 15 18 9"></polyline>
-                                                    </svg></div>
+                                                    </svg>
+                                                </div>
                                             </div>
                                         </section>
                                     </div>
 
-                                    <div id="defaultAccordionOne" class="collapse" aria-labelledby="headingOne1" data-parent="#toggleAccordion">
+                                    <div id="<?php echo $row->kb_id;?>" class="collapse" aria-labelledby="headingOne3" data-parent="#iconsAccordion">
                                         <div class="card-body">
                                             <p class="">
-                                                Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-                                            </p>
-
-                                            <p class="">
-                                                Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                                                <?php echo $kb_desc;?>
                                             </p>
                                         </div>
                                     </div>
                                 </div>
-                                </section>
-                            </div>
-                            <div id="iconAccordionOne" class="collapse" aria-labelledby="..." data-parent="#iconsAccordion">
-                                <div class="card-body">
-
-                                    ..................
-                                    ..................
-
-                                </div>
-                            </div>
+                            <?php } ?>
                         </div>
-
                     </div>
                 </div>
             </div>
         </div>
-    </div>
-    <?php
-    require_once('partials/_footer.php');
-    ?>
+        <?php
+        require_once('partials/_footer.php');
+        ?>
     </div>
     <!--  END CONTENT AREA  -->
     </div>
