ecf1a020e4532036909553293b21a747e1ed9a01
MartMbithi <martdevelopers254@gmail.com>
2020-09-17 00:18:40 +0300

    Delete On Payment Methods

---
+++ resources/views/core/admin/payment_means.php
@@ -0,0 +1,150 @@
+<?php
+session_start();
+require_once('configs/config.php');
+require_once('configs/checklogin.php');
+
+if (isset($_GET['delete'])) {
+    $delete = $_GET['delete'];
+    $adn = "DELETE FROM payment_methods WHERE method_id =?";
+    $stmt = $mysqli->prepare($adn);
+    $stmt->bind_param('s', $delete);
+    $stmt->execute();
+    $stmt->close();
+    if ($stmt) {
+        $success = "Deleted" && header("refresh:1; url=payment_means.php");
+    } else {
+        $info = "Please Try Again Or Try Later";
+    }
+}
+require_once('partials/_head.php');
+?>
+
+<body>
+
+    <!--  BEGIN NAVBAR  -->
+    <?php require_once('partials/_nav.php'); ?>
+    <!--  END NAVBAR  -->
+
+    <!--  BEGIN NAVBAR  -->
+    <div class="sub-header-container">
+        <header class="header navbar navbar-expand-sm">
+            <a href="javascript:void(0);" class="sidebarCollapse" data-placement="bottom"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-menu">
+                    <line x1="3" y1="12" x2="21" y2="12"></line>
+                    <line x1="3" y1="6" x2="21" y2="6"></line>
+                    <line x1="3" y1="18" x2="21" y2="18"></line>
+                </svg></a>
+
+            <ul class="navbar-nav flex-row">
+                <li>
+                    <div class="page-header">
+
+                        <nav class="breadcrumb-one" aria-label="breadcrumb">
+                            <ol class="breadcrumb">
+                                <li class="breadcrumb-item"><a href="dashboard.php">Home</a></li>
+                                <li class="breadcrumb-item"><a href="dashboard.php">Dashboard</a></li>
+                                <li class="breadcrumb-item"><a href="">Billings</a></li>
+                                <li class="breadcrumb-item active" aria-current="page"><span>Payment Means</span></li>
+                            </ol>
+                        </nav>
+
+                    </div>
+                </li>
+            </ul>
+        </header>
+    </div>
+    <!--  END NAVBAR  -->
+
+    <!--  BEGIN MAIN CONTAINER  -->
+    <div class="main-container" id="container">
+
+        <div class="overlay"></div>
+        <div class="search-overlay"></div>
+
+        <!--  BEGIN SIDEBAR  -->
+        <div class="sidebar-wrapper sidebar-theme">
+
+            <?php require_once('partials/_sidebar.php'); ?>
+
+        </div>
+        <!--  END SIDEBAR  -->
+
+        <!--  BEGIN CONTENT AREA  -->
+        <div id="content" class="main-content">
+            <div class="layout-px-spacing">
+
+                <div class="row layout-top-spacing">
+                    <div class="col-xl-12 col-lg-12 col-sm-12  layout-spacing">
+                        <div class="widget-content widget-content-area br-6">
+
+                            <a class="btn btn-outline-success" href="add_package.php">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-activity">
+                                    <path d="M12.89 1.45l8 4A2 2 0 0 1 22 7.24v9.53a2 2 0 0 1-1.11 1.79l-8 4a2 2 0 0 1-1.79 0l-8-4a2 2 0 0 1-1.1-1.8V7.24a2 2 0 0 1 1.11-1.79l8-4a2 2 0 0 1 1.78 0z"></path>
+                                    <polyline points="2.32 6.16 12 11 21.68 6.16"></polyline>
+                                    <line x1="12" y1="22.76" x2="12" y2="11"></line>
+                                    <line x1="7" y1="3.5" x2="17" y2="8.5"></line>
+                                </svg>
+
+                                Add New Payment Method
+                            </a>
+
+                            <!--<a class="btn btn-outline-primary" href="">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-activity">
+                                    <path d="M16 21v-2a4 4 0 0 0-4-4H5a4 4 0 0 0-4 4v2"></path>
+                                    <circle cx="8.5" cy="7" r="4"></circle>
+                                    <polyline points="17 11 19 13 23 9"></polyline>
+                                </svg>
+                                Import .XLS Document
+                            </a>-->
+                            <div class="table-responsive mb-4 mt-4">
+                                <table id="zero-config" class="table table-hover" style="width:100%" style="width:100%">
+                                    <thead>
+                                        <tr>
+                                            <th>Code</th>
+                                            <th>Name</th>
+                                            <th>Generated On</th>
+                                            <th>Action</th>
+                                        </tr>
+                                    </thead>
+
+                                    <tbody>
+                                        <?php
+                                        $ret = "SELECT * FROM `payment_methods` ";
+                                        $stmt = $mysqli->prepare($ret);
+                                        $stmt->execute(); //ok
+                                        $res = $stmt->get_result();
+                                        while ($row = $res->fetch_object()) {
+                                        ?>
+                                            <tr>
+                                                <td>
+                                                    <span class="badge outline-badge-success">
+                                                        <?php echo $row->package_id; ?>
+                                                    </span>
+                                                </td>
+                                                <td><?php echo $row->package_name; ?></td>
+                                                <td>Ksh <?php echo $row->package_price; ?></td>
+                                                <td><?php echo date('d M Y g:i', strtotime($row->created_at)); ?></td>
+                                                <td>
+                                                    <a class="badge outline-badge-primary" href="update_package.php?update=<?php echo $row->package_id; ?>">Update</a>
+
+                                                    <a class="badge outline-badge-danger text-danger" href="packages.php?delete=<?php echo $row->package_id; ?>">Delete</a>
+                                                </td>
+                                            </tr>
+                                        <?php
+                                        } ?>
+                                    </tbody>
+                                </table>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+            <?php require_once('partials/_footer.php'); ?>
+        </div>
+        <!--  END CONTENT AREA  -->
+    </div>
+    <!-- END MAIN CONTAINER -->
+    <?php require_once('partials/_scripts.php'); ?>
+</body>
+
+</html>
