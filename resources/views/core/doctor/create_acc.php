be9403fdf7f1194bf3bdafa660fa78fc954ce579
MartMbithi <martdevelopers254@gmail.com>
2020-09-19 12:23:56 +0300

    Minor Password Harsh Issues

--- resources/views/core/doctor/create_acc.php
+++ resources/views/core/doctor/create_acc.php
@@ -11,10 +11,11 @@ if (isset($_POST['sign-up'])) {
     $doc_email = $_POST['doc_email'];
     $doc_phone = $_POST['doc_phone'];
     $doc_status = $_POST['doc_status'];
+    $doc_password = sha1(md5($_POST['doc_password']));
 
-    $query = "INSERT INTO medical_experts (month_joined, doc_id, doc_number, doc_name, doc_email, doc_phone, doc_status) VALUES (?,?,?,?,?,?,?)";
+    $query = "INSERT INTO medical_experts (doc_password, month_joined, doc_id, doc_number, doc_name, doc_email, doc_phone, doc_status) VALUES (?,?,?,?,?,?,?,?)";
     $stmt = $mysqli->prepare($query);
-    $rc = $stmt->bind_param('sssssss', $month_joined, $doc_id, $doc_number, $doc_name, $doc_email, $doc_phone, $doc_status);
+    $rc = $stmt->bind_param('ssssssss', $doc_password, $month_joined, $doc_id, $doc_number, $doc_name, $doc_email, $doc_phone, $doc_status);
     $stmt->execute();
     if ($stmt) {
         //inject alert that post is shared  
@@ -55,13 +56,13 @@ require_once('partials/_head.php');
 
                                 <div id="username-field" class="field-wrapper input">
                                     <label for="username">Phone Number</label>
-                                    <input id="username" required name="doc_phone" type="text" class="form-control">
+                                    <input  required name="doc_phone" type="text" class="form-control">
                                 </div>
                                 <div id="password-field" class="field-wrapper input mb-2">
                                     <div class="d-flex justify-content-between">
                                         <label for="password">Password</label>
                                     </div>
-                                    <input id="password" required name="admin_password" type="password" class="form-control">
+                                    <input id="password" required name="doc_password" type="password" class="form-control">
                                     <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" id="toggle-password" class="feather feather-eye">
                                         <path d="M1 12s4-8 11-8 11 8 11 8-4 8-11 8-11-8-11-8z"></path>
                                         <circle cx="12" cy="12" r="3"></circle>
