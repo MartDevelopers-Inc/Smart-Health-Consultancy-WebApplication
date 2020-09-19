25c78938d50e8edecf417e40408de4861f78d9f0
MartMbithi <martdevelopers254@gmail.com>
2020-09-19 12:25:19 +0300

    Updated Check Login Function

--- resources/views/core/doctor/configs/checklogin.php
+++ resources/views/core/doctor/configs/checklogin.php
@@ -1,12 +1,12 @@
 <?php
 function check_login()
 {
-	if(strlen($_SESSION['admin_id'])==0)
+	if(strlen($_SESSION['doc_id'])==0)
 		{
 			$host = $_SERVER['HTTP_HOST'];
 			$uri  = rtrim(dirname($_SERVER['PHP_SELF']), '/\\');
 			$extra="index.php";
-			$_SESSION["admin_id"]="";
+			$_SESSION["doc_id"]="";
 			header("Location: http://$host$uri/$extra");
 			
 		}
