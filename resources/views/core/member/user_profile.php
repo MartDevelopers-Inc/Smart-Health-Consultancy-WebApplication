a3efb0d4d9864198e5586441aa4b3b4fc69d0a04
MartMbithi <martdevelopers254@gmail.com>
2020-09-20 13:28:26 +0300

    Profile Update

--- resources/views/core/member/user_profile.php
+++ resources/views/core/member/user_profile.php
@@ -77,9 +77,9 @@ require_once('partials/_head.php');
                                     <div class="text-center user-info">
                                         <?php
                                         if ($row->member_pic == '') {
-                                            echo "<img src='../assets/img/admin/admin.png' class='img-thumbnail img-fluid' alt='avatar'>";
+                                            echo "<img src='../admin/assets/img/admin/admin.png' class='img-thumbnail img-fluid' alt='avatar'>";
                                         } else {
-                                            echo "<img src='../assets/img/clients/$row->member_pic' class='img-thumbnail img-fluid' alt='avatar'>";
+                                            echo "<img src='../admin/assets/img/clients/$row->member_pic' class='img-thumbnail img-fluid' alt='avatar'>";
                                         }
                                         ?>
                                         <p class=""><?php echo $row->member_name; ?></p>
@@ -117,8 +117,11 @@ require_once('partials/_head.php');
                                                     </svg> <?php echo $row->member_phone; ?>
                                                 </li>
                                                 <li class="contacts-block__item">
-                                                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-phone">
-                                                        <path d="M22 16.92v3a2 2 0 0 1-2.18 2 19.79 19.79 0 0 1-8.63-3.07 19.5 19.5 0 0 1-6-6 19.79 19.79 0 0 1-3.07-8.67A2 2 0 0 1 4.11 2h3a2 2 0 0 1 2 1.72 12.84 12.84 0 0 0 .7 2.81 2 2 0 0 1-.45 2.11L8.09 9.91a16 16 0 0 0 6 6l1.27-1.27a2 2 0 0 1 2.11-.45 12.84 12.84 0 0 0 2.81.7A2 2 0 0 1 22 16.92z"></path>
+                                                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-activity">
+                                                        <path d="M12.89 1.45l8 4A2 2 0 0 1 22 7.24v9.53a2 2 0 0 1-1.11 1.79l-8 4a2 2 0 0 1-1.79 0l-8-4a2 2 0 0 1-1.1-1.8V7.24a2 2 0 0 1 1.11-1.79l8-4a2 2 0 0 1 1.78 0z"></path>
+                                                        <polyline points="2.32 6.16 12 11 21.68 6.16"></polyline>
+                                                        <line x1="12" y1="22.76" x2="12" y2="11"></line>
+                                                        <line x1="7" y1="3.5" x2="17" y2="8.5"></line>
                                                     </svg>
                                                     <?php
                                                     if ($row->member_package == 'Gold Package') {
@@ -177,7 +180,7 @@ require_once('partials/_head.php');
                                         <h3 class="">Consultation History</h3>
                                         <div class="timeline-alter">
                                             <?php
-                                            $ret = "SELECT * FROM `consultations`  WHERE member_id = '$view' ";
+                                            $ret = "SELECT * FROM `consultations`  WHERE member_id = '$member_id' ";
                                             $stmt = $mysqli->prepare($ret);
                                             $stmt->execute(); //ok
                                             $res = $stmt->get_result();
