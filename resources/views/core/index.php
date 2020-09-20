9dbdd16f6d55641d50ff1c90c7864db760567afe
MartMbithi <martdevelopers254@gmail.com>
2020-09-21 01:06:27 +0300

    Landing Pages Finesse

--- resources/views/core/index.php
+++ resources/views/core/index.php
@@ -302,9 +302,8 @@
                     <div class="col-lg-7 col-12">
                         <div class="sepcial_banner_content">
                             <h4>Special Offer !!</h4>
-                            <h2>Get 40% off on Every Dental <br> Checkups.</h2>
-                            <p>There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable.</p>
-                            <a href="#">Learn More</a>
+                            <h2>Get Free Membership <br> For One Month</h2>
+                            <a href="member/">Join Us</a>
                         </div>
                     </div>
                 </div>
