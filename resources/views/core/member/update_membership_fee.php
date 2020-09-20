1ed99a592499e092c7f1776b1a5cb0ce54568af6
MartMbithi <martdevelopers254@gmail.com>
2020-09-20 15:12:09 +0300

    Update Functionality On Membership

--- resources/views/core/member/update_membership_fee.php
+++ resources/views/core/member/update_membership_fee.php
@@ -6,20 +6,15 @@ include('configs/checklogin.php');
 include('configs/codeGen.php');
 check_login();
 
-if (isset($_POST['add'])) {
+if (isset($_POST['update'])) {
 
-    $pay_id = $_POST['pay_id'];
+    $update = $_GET['update'];
     $pay_code = $_POST['pay_code'];
     $pay_method = $_POST['pay_method'];
-    $pay_amt = $_POST['pay_amt'];
-    $status = $_POST['status'];
-    $member_id = $_GET['member_id'];
-    $member_package = $_GET['member_package'];
-    $member_name = $_GET['member_name'];
 
-    $query = "INSERT INTO membership_payments (pay_id, pay_code, pay_method, pay_amt, status, member_id, member_package, member_name) VALUES (?,?,?,?,?,?,?,?)";
+    $query = "UPDATE membership_payments SET pay_code =?, pay_method =? WHERE pay_id =?";
     $stmt = $mysqli->prepare($query);
-    $rc = $stmt->bind_param('ssssssss', $pay_id, $pay_code, $pay_method, $pay_amt, $status, $member_id, $member_package, $member_name);
+    $rc = $stmt->bind_param('sss',  $pay_code, $pay_method, $update);
     $stmt->execute();
     if ($stmt) {
         $success = "Success" && header("refresh:1; url=membership_fee.php");
@@ -59,7 +54,7 @@ require_once('partials/_head.php');
                                 <li class="breadcrumb-item"><a href="dashboard.php">Home</a></li>
                                 <li class="breadcrumb-item"><a href="dashboard.php">Dashboard</a></li>
                                 <li class="breadcrumb-item"><a href="membership_fee.php">Billings</a></li>
-                                <li class="breadcrumb-item active" aria-current="page"><span>Pay Membership Fee</span></li>
+                                <li class="breadcrumb-item active" aria-current="page"><span>Manage Membership Fee</span></li>
                             </ol>
                         </nav>
 
@@ -117,8 +112,8 @@ require_once('partials/_head.php');
                                         <input type="text" name="pay_code" value="<?php echo $paycode; ?>" class="form-control">
                                     </div>
                                     <?php
-                                    $member_package = $_GET['member_package'];
-                                    $ret = "SELECT * FROM `packages` WHERE package_name ='$member_package' ";
+                                    $update = $_GET['update'];
+                                    $ret = "SELECT * FROM `membership_payments` WHERE pay_id ='$update' ";
                                     $stmt = $mysqli->prepare($ret);
                                     $stmt->execute(); //ok
                                     $res = $stmt->get_result();
@@ -127,16 +122,16 @@ require_once('partials/_head.php');
                                     ?>
                                         <div class="form-group col-md-6">
                                             <label for="inputEmail4">Package</label>
-                                            <input type="text" readonly value="<?php echo $row->package_name; ?>" class="form-control">
+                                            <input type="text" readonly value="<?php echo $row->member_package; ?>" class="form-control">
                                         </div>
                                         <div class="form-group col-md-6">
                                             <label for="inputEmail4">Payment Amount</label>
-                                            <input type="text" readonly value="<?php echo $row->package_price; ?>" class="form-control" name="pay_amt">
+                                            <input type="text" readonly value="<?php echo $row->pay_amt; ?>" class="form-control" name="pay_amt">
                                         </div>
                                     <?php } ?>
                                 </div>
 
-                                <button type="submit" name="add" class="btn btn-primary mt-3">Submit</button>
+                                <button type="submit" name="update" class="btn btn-primary mt-3">Submit</button>
                             </form>
                         </div>
                     </div>
