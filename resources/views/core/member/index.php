88d1fdd88ba454d179a4380f37604a27d8eaa11d
MartMbithi <martdevelopers254@gmail.com>
2020-09-20 12:37:18 +0300

    Cranked Login Logic

--- resources/views/core/member/index.php
+++ resources/views/core/member/index.php
@@ -3,14 +3,14 @@ session_start();
 include('configs/config.php');
 //handle login
 if (isset($_POST['login'])) {
-    $doc_email = $_POST['doc_email'];
-    $doc_password = sha1(md5($_POST['doc_password'])); //double encrypt to increase security
-    $stmt = $mysqli->prepare("SELECT doc_email, doc_password, doc_id  FROM medical_experts  WHERE (doc_email =? AND doc_password =?)");
-    $stmt->bind_param('ss', $doc_email, $doc_password); //bind fetched parameters
+    $member_email = $_POST['member_email'];
+    $member_password = sha1(md5($_POST['member_password'])); //double encrypt to increase security
+    $stmt = $mysqli->prepare("SELECT member_email, member_password, member_id  FROM members  WHERE (member_email =? AND member_password =?)");
+    $stmt->bind_param('ss', $member_email, $member_password); //bind fetched parameters
     $stmt->execute(); //execute bind 
-    $stmt->bind_result($doc_email, $doc_password, $doc_id); //bind result
+    $stmt->bind_result($member_email, $member_password, $member_id); //bind result
     $rs = $stmt->fetch();
-    $_SESSION['doc_id'] = $doc_id;
+    $_SESSION['member_id'] = $session;
     if ($rs) {
         //if its sucessfull
         header("location:dashboard.php");
@@ -29,7 +29,7 @@ require_once('partials/_head.php');
             <div class="form-form-wrap">
                 <div class="form-container">
                     <div class="form-content">
-                        <h1 class="">Smart Health Consultancy - Doctors` Module</h1>
+                        <h1 class="">Smart Health Consultancy - Client Area</h1>
                         <p class="">Log in to your account to continue.</p>
                         <form method="POST" class="text-left">
                             <div class="form">
@@ -40,7 +40,7 @@ require_once('partials/_head.php');
                                         <path d="M20 21v-2a4 4 0 0 0-4-4H8a4 4 0 0 0-4 4v2"></path>
                                         <circle cx="12" cy="7" r="4"></circle>
                                     </svg>
-                                    <input id="username" required name="doc_email" type="email" class="form-control">
+                                    <input id="username" required name="member_email" type="email" class="form-control">
                                 </div>
 
                                 <div id="password-field" class="field-wrapper input mb-2">
@@ -52,7 +52,7 @@ require_once('partials/_head.php');
                                         <rect x="3" y="11" width="18" height="11" rx="2" ry="2"></rect>
                                         <path d="M7 11V7a5 5 0 0 1 10 0v4"></path>
                                     </svg>
-                                    <input id="password" required name="doc_password" type="password" class="form-control">
+                                    <input id="password" required name="member_password" type="password" class="form-control">
                                     <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" id="toggle-password" class="feather feather-eye">
                                         <path d="M1 12s4-8 11-8 11 8 11 8-4 8-11 8-11-8-11-8z"></path>
                                         <circle cx="12" cy="12" r="3"></circle>
