42e057ea1a39a64cd593a9e24a8bd8b233dc93f3
MartMbithi <martdevelopers254@gmail.com>
2020-09-22 10:41:04 +0300

    Favicons

--- resources/views/core/partials/_head.php
+++ resources/views/core/partials/_head.php
@@ -7,14 +7,22 @@
     <title>Smart Health Consultancy Inc - Craddle Of Health Care Services</title>
     <meta name="description" content="">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+    <link rel="icon" type="images/favicons/android-chrome-192x192.png" sizes="32x32">
+    <link rel="icon" type="images/favicons/android-chrome-512x512.png" sizes="32x32">
+    <link rel="icon" type="images/favicons/favicon-16x16.png" sizes="32x32">
+    <link rel="icon" type="images/favicons/favicon.ico" sizes="32x32">
+    <link rel="icon" type="images/favicons/android-chrome-192x192.png" sizes="32x32">
+
+    <link rel="manifest" href="images/favicons/site.webmanifest">
 
     <link rel="apple-touch-icon" href="apple-touch-icon.png">
-
     <link rel="stylesheet" href="css/bootstrap.min.css">
     <link rel="stylesheet" href="css/plugin.css">
     <link rel="stylesheet" href="css/default.css">
     <link rel="stylesheet" href="style.css">
     <link rel="stylesheet" href="css/responsive.css">
+
 
     <script src="js/vendor/modernizr-2.8.3.min.js"></script>
 </head>
